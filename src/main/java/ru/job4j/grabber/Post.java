4749369e102ca518fdea9c019e4de7d4de14a4fa
VasiliiOrlov <ov_prog@mail.ru>
2022-06-02 00:34:19 +0300

    Архитектура проекта - Аргегатор Java Вакансий [#260359]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -6,10 +6,10 @@ import java.util.Objects;
 public class Post {
 
     private int id;
-    private final String title;
-    private final String link;
-    private final String description;
-    private final LocalDateTime created;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
 
     public Post(String title, String link, String description, LocalDateTime created) {
         this.title = title;
@@ -24,6 +24,38 @@ public class Post {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
     @Override
