845c6dbc6fe9c55296fc5c59dae4796efa2c600e
VasiliiOrlov <ov_prog@mail.ru>
2022-06-03 20:51:19 +0300

    SQL Select by company [#2760]

---
+++ bd/select.sql
@@ -0,0 +1,31 @@
+CREATE TABLE company
+(
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person
+(
+    id integer NOT NULL,
+    name character varying,
+    company_id integer references company(id),
+    CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+select person.name as name_person, company.name as name_company 
+from person 
+join company 
+on company_id = company.id 
+where company_id != 5;
+
+select company.name, count(company.id)
+from person 
+join company 
+on company_id = company.id
+group by company.id
+having count(company.id) = (select max(count.count) 
+							from (select count(company_id) 
+								  from person 
+								  group by company_id) 
+							as count);
